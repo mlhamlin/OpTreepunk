a861bac0d87fb733a4ddec0d6c088a4372a1749b
Gregory Loden <gregory.loden@gmail.com>
2014-09-30 17:51:04 -0400

    two player test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public KeyCode leftkey;
+	public KeyCode rightkey;
+	public KeyCode upkey;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(upkey))
+			rigidbody2D.velocity = new Vector2(0, 12);
+		if (Input.GetKey(leftkey))
+			rigidbody2D.velocity = new Vector2(-3, rigidbody2D.velocity.y);
+		if (Input.GetKey(rightkey))
+			rigidbody2D.velocity = new Vector2(3, rigidbody2D.velocity.y);
+	}
+}
