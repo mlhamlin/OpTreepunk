411962c9465cb0d68f2acd3fd769b6c267abdd5b
Martha Hamlin <mlhamlin92@gmail.com>
2014-10-02 19:07:32 -0400

    Setting up combat system

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+	int damage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void doAttack (Transform targetLoc, HealthData targetHealth) {
+		//Animate?
+		//Move?
+		//Whatever?
+		targetHealth.Damage (damage);
+	}
+}
---
+++ Assets/Scripts/HealthData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthData : MonoBehaviour {
+
+	int health;
+	public float alive;
+
+	// Use this for initialization
+	void Start () {
+		alive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Damage(int amount) {
+		health = Mathf.Min (0, health - amount);
+		alive = !(health == 0);
+	}
+
+	public void Heal(int amount){
+		if (alive) 
+		{
+			health = Mathf.Min (0, health - amount);
+		}
+	}
+}
