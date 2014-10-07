122df42b94728045ea0ddc4c9ab9afdadc7d3c5e
Kevin Cavanaugh <kevrcav@gmail.com>
2014-10-07 13:24:26 -0400

    Made changes to how Kudzu's camera works

---
+++ Assets/Scripts/KudzuCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KudzuCamera : MonoBehaviour {
+
+	public GameObject follow;
+	public Rect followrect;
+	public Rect moverect;
+
+	// Use this for initialization
+	void Start () {
+		float xadjust = (follow.transform.position.x - followrect.x) / followrect.width;
+		float camx = moverect.x + xadjust * moverect.width;
+		float yadjust = (follow.transform.position.y - followrect.y) / followrect.height;
+		float camy = moverect.y + yadjust * moverect.height;
+		gameObject.transform.position = new Vector3 (camx, camy, -1.0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xadjust = (follow.transform.position.x - followrect.x) / followrect.width;
+		float camx = moverect.x + xadjust*moverect.width;
+		float yadjust = (follow.transform.position.y - followrect.y) / followrect.height;
+		float camy = moverect.y + yadjust * moverect.height;
+		gameObject.transform.position = new Vector3 (camx, camy, -1.0f);
+	}
+}
